5f76eb0e84b0fbdd074025b6fc185d4ce30d6841
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2023-11-08 11:16:32 +0800

    improve responsive

--- partial/header-two.php
+++ partial/header-two.php
@@ -6,28 +6,22 @@
         </div>
         <a href="/login.php" class="btn btn-info navbar-top__btn">ログイン </a>
     </div>
-    <nav class="navbar-simple navbar navbar-expand-lg">
+    <nav class="navbar-simple navbar">
         <div class="container container navbar-simple__container">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">アイドル</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">声優</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">タレント</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">アーティスト</a>
-                    </li>
-                </ul>
-            </div>
+            <ul class="navbar-nav mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">アイドル</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">声優</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">タレント</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">アーティスト</a>
+                </li>
+            </ul>
         </div>
-       
     </nav>
 </header>
