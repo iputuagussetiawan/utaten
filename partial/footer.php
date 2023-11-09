141b7ef12c7fb09ab3e80051883e2a2dc6fb1713
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2023-11-09 10:10:18 +0800

    add link target blank

--- partial/footer.php
+++ partial/footer.php
@@ -67,16 +67,16 @@
                 </a>
                 <div class="row footer__menu-container text-center">
                     <div class="col-2 col-md-2 col-lg-2 footer__menu-link-container">
-                        <a href="/agreement.php" class="footer__menu-link">利用規約</a>
+                        <a href="https://kdm.utaten.com/index/agreement" target="_blank" class="footer__menu-link">利用規約</a>
                     </div>
                     <div class="col-3 col-md-3 col-lg-3  footer__menu-link-container">
-                        <a href="/privacy.php" class="footer__menu-link">プライバシーポリシー</a>
+                        <a href="https://kdm.utaten.com/index/privacy" target="_blank" class="footer__menu-link">プライバシーポリシー</a>
                     </div>
                     <div class="col-3 col-md-3 col-lg-3 footer__menu-link-container">
-                        <a href="/law.php" class="footer__menu-link">特定商取引法に基づく表示</a>
+                        <a href="https://kdm.utaten.com/index/law"  target="_blank" class="footer__menu-link">特定商取引法に基づく表示</a>
                     </div>
                     <div class="col-4 col-md-4 col-lg-4 footer__menu-link-container">
-                        <a href="/inquiry.php" class="footer__menu-link">よくある質問・お問い合わせ</a>
+                        <a href="/inquiry.php" target="_blank" class="footer__menu-link">よくある質問・お問い合わせ</a>
                     </div>
                 </div>
             </div>
